bcdd471988aa2a3ff8bdba5430da54d1a14290c5
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-01-05 18:22:11 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -80,7 +80,7 @@ export default function Index() {
                 height={24}
                 priority
                 /></Link>  
-            <Link href="#" style={{marginLeft: "10px"}}>
+            <Link href="https://wa.me/16723270327?text=Hello%2C%20AngleX Team!" style={{marginLeft: "10px"}}>
                 <Image                
                 src="/image/support-icon.png"
                 alt="earphone"
