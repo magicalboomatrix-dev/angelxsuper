6a15e8a4e72d32939fa1cda470a640c9fc84c7b6
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-01-05 18:06:00 +0530

    Update page.js

--- src/app/exchange/page.js
+++ src/app/exchange/page.js
@@ -189,7 +189,7 @@ export default function Exchange() {
             </div>
             <div className="right">
               <a href="https://wa.me/16723270327?text=Hello%2C%20AngleX Team!">
-              <img src="images/customer-care.png" />
+              <img src="images/support-icon.png" />
               </a>
             </div>
           </header>
