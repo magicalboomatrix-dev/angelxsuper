3f11aff7dfcd721d3c3ef8a1d15953c32de3ffe5
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-01-06 18:31:58 +0530

    Update page.js

--- src/app/USDT-deposit/page.js
+++ src/app/USDT-deposit/page.js
@@ -189,7 +189,7 @@ export default function USDTDeposit() {
                       }
                       ::placeholder {
                         color: gray;
-                        font-size: 12px;
+                        font-size: 15px;
                       }
                     `}</style>
                   </div>
