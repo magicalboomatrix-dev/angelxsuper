6a7028b8ac0fddcc9735eea36c361c80bcf16fbb
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-01 20:59:00 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,38 @@
 import { Geist, Geist_Mono } from "next/font/google";
-//import "./globals.css";
 import Script from "next/script";
 import LayoutClient from "./LayoutClient";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
- export const metadata = {
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
   title: "AngelX Super",
   description: "AngelX is the most trustable exchange partner. Exchange more, earn more!",
   manifest: "/manifest.json",
-  
-}; 
-
+};
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" type="image/x-icon" href="/image/logo-icon.png" />
-        <meta name="google-site-verification" content="WJJgbZoAjQ2OYURThYg5MzQzF8ZIpJwIfitC2E_t6Fg" />
 
+        {/* Google Search Console */}
+        <meta
+          name="google-site-verification"
+          content="WJJgbZoAjQ2OYURThYg5MzQzF8ZIpJwIfitC2E_t6Fg"
+        />
+
+        {/* Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
@@ -30,37 +41,48 @@ export default function Layout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Monda:wght@400..700&display=swap"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+        {/* Styles */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
         <link rel="stylesheet" href="/css/style.css" type="text/css" />
-        <meta name="description" content="" />
-           <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17922858127"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'AW-17922858127');
-</script>
- <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SGHVYRBG7S"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        {/* Google Ads + GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17922858127"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', 'G-SGHVYRBG7S');
-</script>
- 
+            gtag('config', 'AW-17922858127');
+            gtag('config', 'G-SGHVYRBG7S');
+          `}
+        </Script>
       </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <LayoutClient>{children}</LayoutClient>
 
-        <Script src="https://code.jquery.com/jquery-3.6.0.js" strategy="beforeInteractive" />
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" strategy="afterInteractive" />
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js" strategy="afterInteractive" />
+        {/* JS Libraries */}
+        <Script
+          src="https://code.jquery.com/jquery-3.6.0.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
 }
-
