84332b43095c60b0b13c6eb31e2b9b1f4972964f
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-01 18:07:40 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -145,15 +145,6 @@ export default function Index() {
                 priority
                 /></Link>
             </div>
-           <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17922858127"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17922858127');
-</script>      
         </header>
 
         <div className="page-wrapper" style={{paddingTop: '10px'}}>       
